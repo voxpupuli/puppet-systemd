4f8c24f0639f4df0c1453e07642c29b7354bf8dd
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-07-15 21:13:29 -0400

    Break out networkd and resolved into classes
    Fixes the issue that if, for some reason, someone would need to trigger
    either networkd, or resolved independently, they would need to break the
    class abstration and call the Service resources directly.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,8 +9,8 @@
 # @param manage_resolved
 #   Manage the systemd resolver
 #
-# @param resolvd_ensure
-#   The state that the ``resolvd`` service should be in
+# @param resolved_ensure
+#   The state that the ``resolved`` service should be in
 #
 # @param manage_networkd
 #   Manage the systemd network daemon
@@ -21,7 +21,7 @@
 class systemd (
   Optional[Hash]                             $service_limits  = undef,
   Boolean                                    $manage_resolved = true,
-  Variant[Enum['stopped','running'],Boolean] $resolvd_ensure  = 'running',
+  Variant[Enum['stopped','running'],Boolean] $resolved_ensure  = 'running',
   Boolean                                    $manage_networkd = true,
   Variant[Enum['stopped','running'],Boolean] $networkd_ensure = 'running',
 ){
@@ -33,32 +33,10 @@ class systemd (
   }
 
   if $manage_resolved {
-    $_enable_resolvd = $resolvd_ensure ? {
-      /stopped/ => false,
-      /running/ => true,
-      default   => $resolvd_ensure
-    }
-
-    service{ 'systemd-resolved':
-      ensure => $resolvd_ensure,
-      enable => $_enable_resolvd
-    }
-    -> file{ '/etc/resolv.conf':
-      ensure => 'symlink',
-      target => '/run/systemd/resolve/resolv.conf'
-    }
+    contain ::systemd::resolved
   }
 
   if $manage_networkd {
-    $_enable_networkd = $networkd_ensure ? {
-      /stopped/ => false,
-      /running/ => true,
-      default   => $networkd_ensure
-    }
-
-    service{ 'systemd-networkd':
-      ensure => $networkd_ensure,
-      enable => $_enable_networkd
-    }
+    contain ::systemd::networkd
   }
 }
