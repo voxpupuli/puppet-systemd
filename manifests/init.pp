333a362acd0c42e56752d8920b7c345135f988a3
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-07-15 21:00:19 -0400

    rebase and update spec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,24 @@
 #   May be passed a resource hash suitable for passing directly into the
 #   ``create_resources()`` function as called on ``systemd::service_limits``
 #
+# @param manage_resolved
+#   Manage the systemd resolver
+#
+# @param resolvd_ensure
+#   The state that the ``resolvd`` service should be in
+#
+# @param manage_networkd
+#   Manage the systemd network daemon
+#
+# @param networkd_ensure
+#   The state that the ``networkd`` service should be in
+#
 class systemd (
-  Optional[Hash] $service_limits = undef
-  Boolean $manage_resolved = true,
-  Boolean $manage_networkd = true,
+  Optional[Hash]                             $service_limits  = undef,
+  Boolean                                    $manage_resolved = true,
+  Variant[Enum['stopped','running'],Boolean] $resolvd_ensure  = 'running',
+  Boolean                                    $manage_networkd = true,
+  Variant[Enum['stopped','running'],Boolean] $networkd_ensure = 'running',
 ){
 
   contain ::systemd::systemctl::daemon_reload
@@ -19,20 +33,32 @@ class systemd (
   }
 
   if $manage_resolved {
-    service{'systemd-resolved':
-      ensure => 'running',
-      enable => true,
+    $_enable_resolvd = $resolvd_ensure ? {
+      /stopped/ => false,
+      /running/ => true,
+      default   => $resolvd_ensure
     }
-    -> file{'/etc/resolv.conf':
+
+    service{ 'systemd-resolved':
+      ensure => $resolvd_ensure,
+      enable => $_enable_resolvd
+    }
+    -> file{ '/etc/resolv.conf':
       ensure => 'symlink',
-      target => '/run/systemd/resolve/resolv.conf',
+      target => '/run/systemd/resolve/resolv.conf'
     }
   }
 
   if $manage_networkd {
-    service{'systemd-networkd':
-      ensure => 'running',
-      enable => true,
+    $_enable_networkd = $networkd_ensure ? {
+      /stopped/ => false,
+      /running/ => true,
+      default   => $networkd_ensure
+    }
+
+    service{ 'systemd-networkd':
+      ensure => $networkd_ensure,
+      enable => $_enable_networkd
     }
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,6 +9,10 @@ describe 'systemd' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_class('systemd') }
         it { is_expected.to create_class('systemd::systemctl::daemon_reload') }
+        it { is_expected.to create_service('systemd-resolved').with_ensure('running') }
+        it { is_expected.to create_service('systemd-resolved').with_enable(true) }
+        it { is_expected.to create_service('systemd-networkd').with_ensure('running') }
+        it { is_expected.to create_service('systemd-networkd').with_enable(true) }
       end
     end
   end
