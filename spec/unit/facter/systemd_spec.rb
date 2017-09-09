eda3fcad6c1a20c8adca70a8d8524cd351c65bce
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-09-09 17:36:15 +0200

    Reuse the service_provider fact from stdlib

--- spec/unit/facter/systemd_spec.rb
+++ spec/unit/facter/systemd_spec.rb
@@ -1,41 +1,36 @@
 require "spec_helper"
 
 describe Facter::Util::Fact do
-  before {
-    Facter.clear
-  }
+  before { Facter.clear }
+  after { Facter.clear }
 
-  describe "systemd" do
+  describe 'systemd' do
     context 'returns true when systemd present' do
       before do
         Facter.fact(:kernel).stubs(:value).returns(:linux)
+        Facter.add(:service_provider) { setcode { 'systemd' } }
       end
-      let(:facts) { {:kernel => :linux} }
-      it do
-        Facter::Util::Resolution.expects(:exec).with('ps -p 1 -o comm=').returns('systemd')
-        expect(Facter.value(:systemd)).to eq(true)
-      end
+
+      it { expect(Facter.value(:service_provider)).to eq('systemd') }
+      it { expect(Facter.value(:systemd)).to be true }
     end
-      context 'returns false when systemd not present' do
-        before do
-          Facter.fact(:kernel).stubs(:value).returns(:linux)
-        end
-        let(:facts) { {:kernel => :linux} }
-        it do
-          Facter::Util::Resolution.expects(:exec).with('ps -p 1 -o comm=').returns('init')
-          expect(Facter.value(:systemd)).to eq(false)
-        end
+
+    context 'returns false when systemd not present' do
+      before do
+        Facter.fact(:kernel).stubs(:value).returns(:linux)
+        Facter.add(:service_provider) { setcode { 'redhat' } }
+      end
+
+      it { expect(Facter.value(:service_provider)).to eq('redhat') }
+      it { expect(Facter.value(:systemd)).to be false }
     end
 
     context 'returns nil when kernel is not linux' do
       before do
         Facter.fact(:kernel).stubs(:value).returns(:windows)
       end
-      let(:facts) { {:kernel => :windows} }
-      it do
-        Facter::Util::Resolution.expects(:exec).with('ps -p 1 -o comm=').never
-        expect(Facter.value(:systemd)).to be_nil
-      end
+
+      it { expect(Facter.value(:systemd)).to be_nil }
     end
   end
 end
