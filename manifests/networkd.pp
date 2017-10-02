f3b30f87b0fc59f01c333e0e9691f69fdb4992e3
Tim Meusel <tim@bastelfreak.de>
2017-10-03 00:12:46 +0200

    fix typo in the networkd class

--- manifests/networkd.pp
+++ manifests/networkd.pp
@@ -1,6 +1,6 @@
 # **NOTE: THIS IS A [PRIVATE](https://github.com/puppetlabs/puppetlabs-stdlib#assert_private) CLASS**
 #
-# This class provides an abstract way to trigger resolved
+# This class provides an abstract way to trigger systemd-networkd
 #
 # @param ensure
 #   The state that the ``networkd`` service should be in
