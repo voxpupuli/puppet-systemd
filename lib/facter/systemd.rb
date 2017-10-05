cab471c1e83d2b3b8dfe7bb6b8e45995dd3e50fc
Tim Meusel <tim@bastelfreak.de>
2017-10-05 10:36:27 +0200

    add structured fact to list systemd services + state

--- lib/facter/systemd.rb
+++ lib/facter/systemd.rb
@@ -1,6 +1,6 @@
 # Fact: systemd
 #
-# Purpose: 
+# Purpose:
 #   Determine whether systemd is the init system on the node
 #
 # Resolution:
@@ -9,9 +9,9 @@
 # Caveats:
 #
 
-# Fact: systemd-version
+# Fact: systemd_version
 #
-# Purpose: 
+# Purpose:
 #   Determine the version of systemd installed
 #
 # Resolution:
@@ -20,6 +20,16 @@
 # Caveats:
 #
 
+# Fact: systemd_internal_services
+#
+# Purpose:
+#   List all systemd internal real services + their state
+#
+# Resolution:
+#  Check the output of systemctl --version
+#
+# Caveats:
+#
 Facter.add(:systemd) do
   confine :kernel => :linux
   setcode do
@@ -31,5 +41,18 @@ Facter.add(:systemd_version) do
   confine :systemd => true
   setcode do
     Facter::Util::Resolution.exec("systemctl --version | awk '/systemd/{ print $2 }'")
+  end
+end
+
+Facter.add(:systemd_internal_services) do
+  confine :systemd => true
+  setcode do
+    command_output = Facter::Util::Resolution.exec(
+      'systemctl list-unit-files --no-legend --no-pager "systemd-*" -t service --state=enabled,disabled,enabled-runtime,indirect'
+    )
+    lines = command_output.lines.lazy.map { |line| line.split(/\s+/) }
+    lines.each_with_object({}) do |(service, status, *), result|
+      result[service] = status.to_sym
+    end
   end
 end
