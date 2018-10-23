24d11b57515b84b2fc274ef3ce1b4feb3e47f5d4
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-10-23 15:43:47 +0200

    Test services

--- spec/acceptance/systemd_spec.rb
+++ spec/acceptance/systemd_spec.rb
@@ -3,13 +3,28 @@ require 'spec_helper_acceptance'
 describe 'systemd' do
 
   context 'with defaults' do
-    it 'should idempotently run' do
+    it 'should run successfully and idempotently' do
       pp = <<-EOS
         class { 'systemd': }
       EOS
 
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  ['resolved', 'networkd', 'timesyncd', 'accounting'].each do |srv|
+    context "when managing service #{srv}" do
+      it 'should run successfully and idempotently' do
+        pp = <<-EOS
+          class { 'systemd':
+            manage_#{srv}   => true,
+          }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes => true)
+      end
     end
   end
 end
