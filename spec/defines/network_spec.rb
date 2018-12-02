341cc4093fbbf329e840b7bce27dc8f76c4bb6de
Simon Deziel <simon@sdeziel.info>
2018-12-01 20:21:16 -0500

    Add some test for systemd::network

--- spec/defines/network_spec.rb
+++ spec/defines/network_spec.rb
@@ -1,21 +1,59 @@
 require 'spec_helper'
 
 describe 'systemd::network' do
-  let :params do
-    {
-      restart_service: true
-    }
-  end
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        # manage systemd-networkd service
+        let :pre_condition do
+          "class { 'systemd':
+            manage_networkd => true,
+          }"
+        end
 
-  let(:title) { 'eth0.network' }
+        let(:facts) { facts }
 
-  on_supported_os.each do |os, facts|
-    let :facts do
-      facts
-    end
+        let(:title) { 'eth0.network' }
 
-    context 'with all defaults' do
-      it { is_expected.to compile.with_all_deps }
+        let(:params) {{
+          :content         => 'random stuff',
+          :restart_service => true,
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to create_file("/etc/systemd/network/#{title}").with(
+          :ensure  => 'file',
+          :content => /#{params[:content]}/,
+          :mode    => '0444'
+        ) }
+
+        it { is_expected.to create_file("/etc/systemd/network/#{title}").that_notifies('Service[systemd-networkd]') }
+
+        context 'with group => systemd-network, mode => 0640 and show_diff => false' do
+          let(:title) { 'wg0.netdev' }
+
+          let(:params) {{
+            :content         => 'secret string',
+            :group           => 'systemd-network',
+            :mode            => '0640',
+            :show_diff       => false,
+            :restart_service => true,
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to create_file("/etc/systemd/network/#{title}").with(
+            :ensure    => 'file',
+            :content   => /#{params[:content]}/,
+            :group     => 'systemd-network',
+            :mode      => '0640',
+            :show_diff => false
+          ) }
+
+          it { is_expected.to create_file("/etc/systemd/network/#{title}").that_notifies('Service[systemd-networkd]') }
+        end
+      end
     end
   end
 end
