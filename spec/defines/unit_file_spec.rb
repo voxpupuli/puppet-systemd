90d328bdb69f1be2173b90f01dcc130276f3d654
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-23 19:43:50 +0200

    Clean up unit_file_spec test
    This uses a shorter syntax where possible. It also combines expectations
    on the same resource to reduce the number of catalog compilations.

--- spec/defines/unit_file_spec.rb
+++ spec/defines/unit_file_spec.rb
@@ -9,46 +9,42 @@ describe 'systemd::unit_file' do
         let(:title) { 'test.service' }
 
         let(:params) {{
-          :content => 'random stuff'
+          content: 'random stuff'
         }}
 
         it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to create_file("/etc/systemd/system/#{title}").with(
-          :ensure  => 'file',
-          :content => /#{params[:content]}/,
-          :mode    => '0444'
-        ) }
-
-        it { is_expected.to create_file("/etc/systemd/system/#{title}").that_notifies('Class[systemd::systemctl::daemon_reload]') }
+        it do
+          is_expected.to create_file("/etc/systemd/system/#{title}")
+            .with_ensure('file')
+            .with_content(/#{params[:content]}/)
+            .with_mode('0444')
+            .that_notifies('Class[systemd::systemctl::daemon_reload]')
+        end
 
         context 'with a bad unit type' do
           let(:title) { 'test.badtype' }
 
-          it {
-            expect{
-              is_expected.to compile.with_all_deps
-            }.to raise_error(/expects a match for Systemd::Unit/)
-          }
+          it { is_expected.to compile.and_raise_error(/expects a match for Systemd::Unit/) }
         end
 
         context 'with enable => true and active => true' do
           let(:params) do
-            super().merge({
-              :enable => true,
-              :active => true
-            })
+            super().merge(
+              enable: true,
+              active: true
+            )
           end
 
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_service('test.service').with(
-            :ensure   => true,
-            :enable   => true,
-            :provider => 'systemd'
-          ) }
-
-          it { is_expected.to contain_service('test.service').that_subscribes_to("File[/etc/systemd/system/#{title}]") }
-          it { is_expected.to contain_service('test.service').that_requires('Class[systemd::systemctl::daemon_reload]') }
+          it do
+            is_expected.to contain_service('test.service')
+							.with_ensure(true)
+							.with_enable(true)
+							.with_provider('systemd')
+							.that_subscribes_to("File[/etc/systemd/system/#{title}]")
+							.that_requires('Class[systemd::systemctl::daemon_reload]')
+          end
         end
 
         context 'ensure => absent' do
