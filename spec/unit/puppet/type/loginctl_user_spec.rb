eb14648d0dd8ab1301cdbc07bd75f7e4610da859
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-01-07 10:22:08 +0100

    Add to type specs

--- spec/unit/puppet/type/loginctl_user_spec.rb
+++ spec/unit/puppet/type/loginctl_user_spec.rb
@@ -3,4 +3,21 @@ require 'spec_helper'
 loginctl_user = Puppet::Type.type(:loginctl_user)
 
 describe loginctl_user do
+  let(:common_params) do
+    {
+      title: 'foo',
+    }
+  end
+
+  it 'should disable linger by default' do
+    resource = Puppet::Type.type(:loginctl_user).new(common_params)
+    expect(resource).not_to be_nil
+    expect(resource.parameters[:linger].value).to eq(:disabled)
+  end
+
+  it 'should enable linger when requested' do
+    resource = Puppet::Type.type(:loginctl_user).new(common_params.merge({linger: 'enabled'}))
+    expect(resource).not_to be_nil
+    expect(resource.parameters[:linger].value).to eq(:enabled)
+  end
 end
