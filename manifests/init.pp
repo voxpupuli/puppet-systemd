voxpupuli/puppet-systemd

commit 90cc85cbccc00cc51b775639eb9c20293446e63e

add factory for dropin files (#149)
* add factory for dropin files

* style add colon

Co-authored-by: Christoph Maser <christoph.maser@1und1.de>
Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>

manifests/init.pp
@@ -81,6 +81,8 @@
 # @param logind_settings
 #   Config Hash that is used to configure settings in logind.conf
 #
+# @param drpoin_files
+#   Configure dropin files via hiera with factory pattern
 class systemd (
   Hash[String,Hash[String, Any]]                         $service_limits,
   Boolean                                                $manage_resolved,
@@ -108,6 +110,7 @@ class systemd (
   Systemd::JournaldSettings                              $journald_settings,
   Boolean                                                $manage_logind,
   Systemd::LogindSettings                                $logind_settings,
+  Hash                                                   $dropin_files = {},
 ){
 
   contain systemd::systemctl::daemon_reload
@@ -136,5 +139,11 @@ class systemd (
 
   if $manage_logind {
     contain systemd::logind
+  }
+
+  $dropin_files.each |$name, $resource| {
+    systemd::dropin_file { $name:
+      * => $resource,
+    }
   }
 }

spec/classes/init_spec.rb
@@ -294,6 +294,20 @@ describe 'systemd' do
             )
           }
         end
+        context 'when passing dropin_files' do
+          let(:params) do
+            {
+              dropin_files: {
+                'my-foo.conf' => {
+                  'unit'    => 'foo.service',
+                  'content' => '[Service]\nReadWritePaths=/',
+                },
+              },
+            }
+          end
+
+          it { is_expected.to contain_systemd__dropin_file('my-foo.conf').with_content('[Service]\nReadWritePaths=/') }
+        end
       end
     end
   end

commit 5bc1e27e3eab891e7dd765c7f1a785f0509c6154

Fix typo in parameter name in class documentation
90cc85cbccc00cc51b775639eb9c20293446e63e introduced this but with a typo in the
name so puppet-strings couldn't find it.

manifests/init.pp
@@ -81,7 +81,7 @@
 # @param logind_settings
 #   Config Hash that is used to configure settings in logind.conf
 #
-# @param drpoin_files
+# @param dropin_files
 #   Configure dropin files via hiera with factory pattern
 class systemd (
   Hash[String,Hash[String, Any]]                         $service_limits,