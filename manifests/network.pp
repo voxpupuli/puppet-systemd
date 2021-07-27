301b756140aa2669aa6c14dd846813de69136cdc
Tim Meusel <tim@bastelfreak.de>
2021-07-27 13:26:35 +0200

    Add puppet-strings documentation for systemd::network

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,4 +1,17 @@
 # @summary Creates network config for systemd-networkd
+#
+# @param ensure configure if the file should be configured or deleted
+# @param path directory where the network configs are stored
+# @param content the content of the file
+# @param source a path to a file that's used as source
+# @param target optional absolute path  in case the file should be stored somewhere else
+# @param owner the user who owns the file
+# @param group the group that owns the file
+# @param mode the mode of the file
+# @param show_diff whether the file diff should be shown on modifications
+# @param restart_service whether systemd-networkd should be restarted on changes, defaults to true. `$systemd::manage_networkd` needs to be true as well
+#
+# @author Tim Meusel <tim@bastelfreak.de>
 define systemd::network (
   Enum['file', 'absent']         $ensure          = file,
   Stdlib::Absolutepath           $path            = '/etc/systemd/network',
