e9c449c71511c4fc54dad53e96c74d3d86b88419
Tim Meusel <tim@bastelfreak.de>
2021-07-27 15:43:51 +0200

    Add option to purge non-managed networkd files

--- manifests/networkd.pp
+++ manifests/networkd.pp
@@ -1,12 +1,14 @@
 # @api private
 #
-# This class provides an abstract way to trigger systemd-networkd
+# @summary This class provides an abstract way to trigger systemd-networkd
 #
-# @param ensure
-#   The state that the ``networkd`` service should be in
-#
+# @param ensure The state that the ``networkd`` service should be in
+# @param path path where all networkd files are placed in
+# @param manage_all_network_files if enabled, all networkd files that aren't managed by puppet will be purged
 class systemd::networkd (
   Enum['stopped','running'] $ensure = $systemd::networkd_ensure,
+  Stdlib::Absolutepath $path = $systemd::network_path,
+  Boolean $manage_all_network_files = $systemd::manage_all_network_files,
 ) {
   assert_private()
 
@@ -19,5 +21,15 @@ class systemd::networkd (
   service { 'systemd-networkd':
     ensure => $ensure,
     enable => $_enable_networkd,
+  }
+  # this directory is created by systemd
+  # we define it here to purge non-managed files
+  if $manage_all_network_files {
+    file { $path:
+      ensure  => 'directory',
+      recurse => true,
+      purge   => true,
+      notify  => Service['systemd-networkd'],
+    }
   }
 }
