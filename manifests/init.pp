voxpupuli/puppet-systemd

commit ff098e030b95fa95131bb8e65585c8bdecc23a3d

Migrate static data from hiera to init.pp
This allows us to get rid of one file and represents the data within the
REFERENCE.md

manifests/init.pp
@@ -123,50 +123,50 @@
 #   when enabled, the different accounting options (network traffic, IO, CPU util...) are enabled for units
 #
 # @param accounting
-#   Hash of the different accounting options. This highly depends on the used systemd version. The module provides sane defaults.
+#   Hash of the different accounting options. This highly depends on the used systemd version. The module provides sane defaults per operating system.
 #
 # @param purge_dropin_dirs
 #   When enabled, unused directories for dropin files will be purged
 #
 class systemd (
-  Hash[String,Hash[String, Any]]                         $service_limits,
-  Boolean                                                $manage_resolved,
-  Enum['stopped','running']                              $resolved_ensure,
-  Optional[Variant[Array[String],String]]                $dns,
-  Optional[Variant[Array[String],String]]                $fallback_dns,
-  Optional[Variant[Array[String],String]]                $domains,
-  Optional[Variant[Boolean,Enum['resolve']]]             $llmnr,
-  Optional[Variant[Boolean,Enum['resolve']]]             $multicast_dns,
-  Optional[Variant[Boolean,Enum['allow-downgrade']]]     $dnssec,
-  Optional[Variant[Boolean,Enum['yes', 'opportunistic', 'no']]] $dnsovertls,
-  Optional[Variant[Boolean,Enum['no-negative']]]         $cache,
-  Optional[Variant[Boolean,Enum['udp','tcp']]]           $dns_stub_listener,
-  Boolean                                                $use_stub_resolver,
-  Boolean                                                $manage_networkd,
-  Enum['stopped','running']                              $networkd_ensure,
-  Boolean                                                $manage_timesyncd,
-  Enum['stopped','running']                              $timesyncd_ensure,
-  Optional[Variant[Array,String]]                        $ntp_server,
-  Optional[Variant[Array,String]]                        $fallback_ntp_server,
-  Boolean                                                $manage_accounting,
-  Hash[String,String]                                    $accounting,
-  Boolean                                                $purge_dropin_dirs,
-  Boolean                                                $manage_journald,
-  Systemd::JournaldSettings                              $journald_settings,
-  Boolean                                                $manage_udevd,
-  Optional[Variant[Integer,String]]                      $udev_log,
-  Optional[Integer]                                      $udev_children_max,
-  Optional[Integer]                                      $udev_exec_delay,
-  Optional[Integer]                                      $udev_event_timeout,
-  Optional[Enum['early', 'late', 'never']]               $udev_resolve_names,
-  Optional[Variant[Integer,String]]                      $udev_timeout_signal,
-  Boolean                                                $manage_logind,
-  Systemd::LogindSettings                                $logind_settings,
-  Boolean                                                $manage_all_network_files,
-  Stdlib::Absolutepath                                   $network_path,
-  Hash                                                   $loginctl_users = {},
-  Hash                                                   $dropin_files = {},
-  Hash                                                   $udev_rules = {},
+  Hash[String,String]                                 $accounting,
+  Hash[String,Hash[String, Any]]                      $service_limits = {},
+  Boolean                                             $manage_resolved = false,
+  Enum['stopped','running']                           $resolved_ensure = 'running',
+  Optional[Variant[Array[String],String]]             $dns = undef,
+  Optional[Variant[Array[String],String]]             $fallback_dns = undef,
+  Optional[Variant[Array[String],String]]             $domains = undef,
+  Optional[Variant[Boolean,Enum['resolve']]]          $llmnr = undef,
+  Optional[Variant[Boolean,Enum['resolve']]]          $multicast_dns = undef,
+  Optional[Variant[Boolean,Enum['allow-downgrade']]]  $dnssec = undef,
+  Variant[Boolean,Enum['yes', 'opportunistic', 'no']] $dnsovertls = false,
+  Variant[Boolean,Enum['no-negative']]                $cache = false,
+  Optional[Variant[Boolean,Enum['udp','tcp']]]        $dns_stub_listener = undef,
+  Boolean                                             $use_stub_resolver = false,
+  Boolean                                             $manage_networkd = false,
+  Enum['stopped','running']                           $networkd_ensure = 'running',
+  Boolean                                             $manage_timesyncd = false,
+  Enum['stopped','running']                           $timesyncd_ensure = 'running',
+  Optional[Variant[Array,String]]                     $ntp_server = undef,
+  Optional[Variant[Array,String]]                     $fallback_ntp_server = undef,
+  Boolean                                             $manage_accounting = false,
+  Boolean                                             $purge_dropin_dirs = true,
+  Boolean                                             $manage_journald = true,
+  Systemd::JournaldSettings                           $journald_settings = {},
+  Boolean                                             $manage_udevd = false,
+  Optional[Variant[Integer,String]]                   $udev_log = undef,
+  Optional[Integer]                                   $udev_children_max = undef,
+  Optional[Integer]                                   $udev_exec_delay = undef,
+  Optional[Integer]                                   $udev_event_timeout = undef,
+  Optional[Enum['early', 'late', 'never']]            $udev_resolve_names = undef,
+  Optional[Variant[Integer,String]]                   $udev_timeout_signal = undef,
+  Boolean                                             $manage_logind = false,
+  Systemd::LogindSettings                             $logind_settings = {},
+  Boolean                                             $manage_all_network_files = false,
+  Stdlib::Absolutepath                                $network_path = '/etc/systemd/network',
+  Hash                                                $loginctl_users = {},
+  Hash                                                $dropin_files = {},
+  Hash                                                $udev_rules = {},
 ) {
   create_resources('systemd::service_limits', $service_limits)
 

data/common.yaml
@@ -1,36 +1,0 @@
----
-systemd::service_limits: {}
-systemd::manage_resolved: false
-systemd::resolved_ensure: 'running'
-systemd::dns: ~
-systemd::fallback_dns: ~
-systemd::domains: ~
-systemd::llmnr: ~
-systemd::multicast_dns: ~
-systemd::dnssec: ~
-systemd::dnsovertls: false
-systemd::cache: false
-systemd::dns_stub_listener: ~
-systemd::use_stub_resolver: false
-systemd::manage_networkd: false
-systemd::networkd_ensure: 'running'
-systemd::manage_timesyncd: false
-systemd::timesyncd_ensure: 'running'
-systemd::ntp_server: ~
-systemd::fallback_ntp_server: ~
-systemd::manage_accounting: false
-systemd::accounting: {}
-systemd::purge_dropin_dirs: true
-systemd::manage_journald: true
-systemd::journald_settings: {}
-systemd::manage_udevd: false
-systemd::udev_log: ~
-systemd::udev_children_max: ~
-systemd::udev_exec_delay: ~
-systemd::udev_event_timeout: ~
-systemd::udev_timeout_signal: ~
-systemd::udev_resolve_names: ~
-systemd::manage_logind: false
-systemd::logind_settings: {}
-systemd::manage_all_network_files: false
-systemd::network_path: '/etc/systemd/network'

commit 0b152de171a895c07b77ddc59c4ec85167d21dd9

Declare a default for $accounting
In ff098e030b95fa95131bb8e65585c8bdecc23a3d the defaults were moved from
data/common.yaml to init.pp but this was missed. That means on
unsupported platforms it no longer works to include the class (which
should be a noop).

Fixes: ff098e030b95fa95131bb8e65585c8bdecc23a3d

manifests/init.pp
@@ -141,7 +141,7 @@
 #   When enabled, unused directories for dropin files will be purged
 #
 class systemd (
-  Hash[String,String]                                 $accounting,
+  Hash[String,String]                                 $accounting = {},
   Hash[String[1],Hash[String[1], Any]]                $service_limits = {},
   Hash[String[1],Hash[String[1], Any]]                $networks = {},
   Hash[String[1],Hash[String[1], Any]]                $timers = {},