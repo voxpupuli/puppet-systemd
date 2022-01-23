3e9648ab68a5d4ad9dac3fbe09a9d0b4d85d5beb
Jan-Otto Kröpke <joe@adorsys.de>
2022-01-23 22:24:00 +0100

    Mock systemd

--- spec/functions/systemd_escape_spec.rb
+++ spec/functions/systemd_escape_spec.rb
@@ -3,24 +3,102 @@
 require 'spec_helper'
 describe 'systemd::systemd_escape' do
   context 'with path false' do
-    it { is_expected.to run.with_params('foo', false).and_return('foo') }
-    it { is_expected.to run.with_params('foo/bar/.', false).and_return('foo-bar-.') }
-    it { is_expected.to run.with_params('/foo/bar/', false).and_return('-foo-bar-') }
-    it { is_expected.to run.with_params('//foo//bar//', false).and_return('--foo--bar--') }
-    it { is_expected.to run.with_params('//foo//bar-baz//', false).and_return('--foo--bar\x2dbaz--') }
-    it { is_expected.to run.with_params('//foo:bar,foo_bar.//', false).and_return('--foo:bar\x2cfoo_bar.--') }
-    it { is_expected.to run.with_params('.foo', false).and_return('\x2efoo') }
-    it { is_expected.to run.with_params('abcöäüß', false).and_return('abc\xc3\xb6\xc3\xa4\xc3\xbc\xc3\x9f') }
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['foo']]], { combine: false, failonfail: true }).and_return("foo\n")
+
+      is_expected.to run.with_params('foo', false).and_return('foo')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['foo/bar/.']]], { combine: false, failonfail: true }).and_return("foo-bar-.\n")
+
+      is_expected.to run.with_params('foo/bar/.', false).and_return('foo-bar-.')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['/foo/bar/']]], { combine: false, failonfail: true }).and_return("-foo-bar-\n")
+
+      is_expected.to run.with_params('/foo/bar/', false).and_return('-foo-bar-')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['//foo//bar//']]], { combine: false, failonfail: true }).and_return("--foo--bar--\n")
+
+      is_expected.to run.with_params('//foo//bar//', false).and_return('--foo--bar--')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['//foo//bar-baz//']]], { combine: false, failonfail: true }).and_return("--foo--bar\\x2dbaz--\n")
+
+      is_expected.to run.with_params('//foo//bar-baz//', false).and_return('--foo--bar\x2dbaz--')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['//foo:bar,foo_bar.//']]], { combine: false, failonfail: true }).and_return("--foo:bar\\x2cfoo_bar.--\n")
+
+      is_expected.to run.with_params('//foo:bar,foo_bar.//', false).and_return('--foo:bar\x2cfoo_bar.--')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['.foo']]], { combine: false, failonfail: true }).and_return("\\x2efoo\n")
+
+      is_expected.to run.with_params('.foo', false).and_return('\x2efoo')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [['abcöäüß']]], { combine: false, failonfail: true }).and_return("abc\\xc3\\xb6\\xc3\\xa4\\xc3\\xbc\\xc3\\x9f\n")
+
+      is_expected.to run.with_params('abcöäüß', false).and_return('abc\xc3\xb6\xc3\xa4\xc3\xbc\xc3\x9f')
+    end
   end
 
   context 'with path true' do
-    it { is_expected.to run.with_params('foo', true).and_return('foo') }
-    it { is_expected.to run.with_params('foo/bar/.', true).and_raise_error(Puppet::ExecutionFailure, %r{Execution of 'systemd-escape --path foo/bar/.' returned 1: }) }
-    it { is_expected.to run.with_params('/foo/bar/', true).and_return('foo-bar') }
-    it { is_expected.to run.with_params('//foo//bar//', true).and_return('foo-bar') }
-    it { is_expected.to run.with_params('//foo//bar-baz//', true).and_return('foo-bar\x2dbaz') }
-    it { is_expected.to run.with_params('//foo:bar,foo_bar.//', true).and_return('foo:bar\x2cfoo_bar.') }
-    it { is_expected.to run.with_params('.foo', true).and_return('\x2efoo') }
-    it { is_expected.to run.with_params('abcöäüß', true).and_return('abc\xc3\xb6\xc3\xa4\xc3\xbc\xc3\x9f') }
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path foo]]], { combine: false, failonfail: true }).and_return("foo\n")
+
+      is_expected.to run.with_params('foo', true).and_return('foo')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path foo/bar/.]]], { combine: false, failonfail: true }).and_raise(Puppet::ExecutionFailure, "Execution of 'systemd-escape --path foo/bar/.' returned 1: ")
+
+      is_expected.to run.with_params('foo/bar/.', true).and_raise_error(Puppet::ExecutionFailure, %r{Execution of 'systemd-escape --path foo/bar/.' returned 1: })
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path /foo/bar/]]], { combine: false, failonfail: true }).and_return("foo-bar\n")
+
+      is_expected.to run.with_params('/foo/bar/', true).and_return('foo-bar')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path //foo//bar//]]], { combine: false, failonfail: true }).and_return("foo-bar\n")
+
+      is_expected.to run.with_params('//foo//bar//', true).and_return('foo-bar')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path //foo//bar-baz//]]], { combine: false, failonfail: true }).and_return("foo-bar\\x2dbaz\n")
+
+      is_expected.to run.with_params('//foo//bar-baz//', true).and_return('foo-bar\x2dbaz')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path //foo:bar,foo_bar.//]]], { combine: false, failonfail: true }).and_return("foo:bar\\x2cfoo_bar.\n")
+
+      is_expected.to run.with_params('//foo:bar,foo_bar.//', true).and_return('foo:bar\x2cfoo_bar.')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path .foo]]], { combine: false, failonfail: true }).and_return("\\x2efoo\n")
+
+      is_expected.to run.with_params('.foo', true).and_return('\x2efoo')
+    end
+
+    it do
+      allow(Puppet::Util::Execution).to receive(:execute).with(['systemd-escape', [%w[--path abcöäüß]]], { combine: false, failonfail: true }).and_return("abc\\xc3\\xb6\\xc3\\xa4\\xc3\\xbc\\xc3\\x9f\n")
+
+      is_expected.to run.with_params('abcöäüß', true).and_return('abc\xc3\xb6\xc3\xa4\xc3\xbc\xc3\x9f')
+    end
   end
 end
