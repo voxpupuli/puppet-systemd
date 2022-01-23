b09f5d9866f982d9b0b2bf710e7abf9a9d949968
Jan-Otto Kröpke <joe@adorsys.de>
2022-01-23 18:59:06 +0100

    fix tests

--- spec/functions/systemd_escape_spec.rb
+++ spec/functions/systemd_escape_spec.rb
@@ -15,10 +15,10 @@ describe 'systemd::systemd_escape' do
 
   context 'with path true' do
     it { is_expected.to run.with_params('foo', true).and_return('foo') }
-    it { is_expected.to run.with_params('foo/bar/.', true).and_raise_error(%r{ path can not end}) }
+    it { is_expected.to run.with_params('foo/bar/.', true).and_raise_error(Puppet::ExecutionFailure, %r{Execution of 'systemd-escape --path foo/bar/.' returned 1: }) }
     it { is_expected.to run.with_params('/foo/bar/', true).and_return('foo-bar') }
     it { is_expected.to run.with_params('//foo//bar//', true).and_return('foo-bar') }
-    it { is_expected.to run.with_params('//foo//bar-baz//', false).and_return('foo-bar\x2dbaz') }
+    it { is_expected.to run.with_params('//foo//bar-baz//', true).and_return('foo-bar\x2dbaz') }
     it { is_expected.to run.with_params('//foo:bar,foo_bar.//', true).and_return('foo:bar\x2cfoo_bar.') }
     it { is_expected.to run.with_params('.foo', true).and_return('\x2efoo') }
     it { is_expected.to run.with_params('abcöäüß', true).and_return('abc\xc3\xb6\xc3\xa4\xc3\xbc\xc3\x9f') }
