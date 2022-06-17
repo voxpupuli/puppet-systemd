2c58e0d8b58dc253e932740d0583a7bbd2e00ac6
Trevor Vaughan <tvaughan@onyxpoint.com>
2022-06-17 16:40:06 -0400

    Update manifests/daemon_reload.pp
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- manifests/daemon_reload.pp
+++ manifests/daemon_reload.pp
@@ -11,7 +11,7 @@
 #   * Added in case users want to disable the reload globally using a resource collector
 #
 define systemd::daemon_reload (
-  Boolean $enable = true
+  Boolean $enable = true,
 ) {
   if $enable {
     exec { "${module_name}-${name}-systemctl-daemon-reload":
