voxpupuli/puppet-systemd

commit 8a9e03a4d6a58a5a5083d7ead60dcbc87f727d2f

Remove restart_service on service_limits define
Since 97dd16fa32886b5b0f77a6f38a4953d4c1511c0e this parameter is a bad
idea. It doesn't do a daemon reload and it may restart at the wrong
time. In fact, I'd argue it's always been a bad idea.

The recommended alternative to this is to manage the service explicitly
and let Puppet handle it. There is an automatic notify that takes care
of it.

Fixes #190

manifests/service_limits.pp
@@ -26,16 +26,12 @@
 #
 #   * Mutually exclusive with ``$limits``
 #
-# @param restart_service
-#   Restart the managed service after setting the limits
-#
 define systemd::service_limits (
   Enum['present', 'absent', 'file'] $ensure                  = 'present',
   Stdlib::Absolutepath              $path                    = '/etc/systemd/system',
   Boolean                           $selinux_ignore_defaults = false,
   Optional[Systemd::ServiceLimits]  $limits                  = undef,
   Optional[String]                  $source                  = undef,
-  Boolean                           $restart_service         = true
 ) {
   include systemd
 
@@ -67,15 +63,6 @@ define systemd::service_limits (
     selinux_ignore_defaults => $selinux_ignore_defaults,
     content                 => $_content,
     source                  => $source,
-  }
-
-  if $restart_service {
-    exec { "restart ${name} because limits":
-      command     => "systemctl restart ${name}",
-      path        => $facts['path'],
-      refreshonly => true,
-    }
-
-    Systemd::Dropin_file["${name}-90-limits.conf"] ~> Exec["restart ${name} because limits"]
+    notify_service          => true,
   }
 }

spec/defines/service_limits_spec.rb
@@ -51,12 +51,21 @@ describe 'systemd::service_limits' do
               with_content(%r{IOReadBandwidthMax=/bw/max 10K})
           }
 
-          it {
-            expect(subject).to create_exec("restart #{title} because limits").with(
-              command: "systemctl restart #{title}",
-              refreshonly: true
-            )
-          }
+          describe 'with service managed' do
+            let(:pre_condition) do
+              <<-PUPPET
+              service { 'test':
+              }
+              PUPPET
+            end
+
+            it { is_expected.to compile.with_all_deps }
+
+            it do
+              is_expected.to create_file("/etc/systemd/system/#{title}.d/90-limits.conf").
+                that_notifies('Service[test]')
+            end
+          end
         end
 
         describe 'ensured absent' do
@@ -66,14 +75,7 @@ describe 'systemd::service_limits' do
 
           it do
             expect(subject).to create_file("/etc/systemd/system/#{title}.d/90-limits.conf").
-              with_ensure('absent').
-              that_notifies("Exec[restart #{title} because limits]")
-          end
-
-          it do
-            expect(subject).to create_exec("restart #{title} because limits").
-              with_command("systemctl restart #{title}").
-              with_refreshonly(true)
+              with_ensure('absent')
           end
         end
       end

commit ace8194b572ff89ab4c5b3a322da4201f16bbf95

Add restart_service parameter on service_limits for compatibility
In 8a9e03a4d6a58a5a5083d7ead60dcbc87f727d2f the parameter was removed,
but this makes it very hard for modules to be compatible with both < 4
and >= 4. Requiring >= 4 will be painful, so this adds the parameter
again but only accepts the parameter false.

A concrete example is the puppet-redis module which sets restart_service
to false to prevent a restart. This would be compatible with both the
old and new behavior, making upgrades smoother.

Fixes: 8a9e03a4d6a58a5a5083d7ead60dcbc87f727d2f

manifests/service_limits.pp
@@ -26,13 +26,21 @@
 #
 #   * Mutually exclusive with ``$limits``
 #
+# @param restart_service
+#   Unused parameter for compatibility with older versions. Will fail if true is passed in.
+#
 define systemd::service_limits (
   Enum['present', 'absent', 'file'] $ensure                  = 'present',
   Stdlib::Absolutepath              $path                    = '/etc/systemd/system',
   Boolean                           $selinux_ignore_defaults = false,
   Optional[Systemd::ServiceLimits]  $limits                  = undef,
   Optional[String]                  $source                  = undef,
+  Boolean                           $restart_service         = false,
 ) {
+  if $restart_service {
+    fail('The restart_service parameter is deprecated and only false is a valid value')
+  }
+
   include systemd
 
   if $name !~ Pattern['^.+\.(service|socket|mount|swap)$'] {