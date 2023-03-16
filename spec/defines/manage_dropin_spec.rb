8f77733117603e4bff2add65c6e543e2ace65b70
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-03-16 15:57:34 +0100

    Add an additional layer of context to manage_dropin test
    This makes it easier to test with multiple unit types. It's mostly a
    whitespace-only change and separating makes it easier to review.

--- spec/defines/manage_dropin_spec.rb
+++ spec/defines/manage_dropin_spec.rb
@@ -10,49 +10,56 @@ describe 'systemd::manage_dropin' do
 
         let(:title) { 'foobar.conf' }
 
-        context 'drop file chaning Type and resetting ExecStart' do
+        context 'on a service' do
           let(:params) do
             {
               unit: 'special.service',
-              service_entry: {
-                Type:      'oneshot',
-                ExecStart: ['', '/usr/bin/doit.sh'],
-              },
             }
           end
 
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_systemd__dropin_file('foobar.conf') }
+          context 'drop file chaning Type and resetting ExecStart' do
+            let(:params) do
+              super().merge(
+                service_entry: {
+                  Type:      'oneshot',
+                  ExecStart: ['', '/usr/bin/doit.sh'],
+                },
+              )
+            end
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              with_content(%r{^\[Service\]$})
-          }
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_systemd__dropin_file('foobar.conf') }
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              without_content(%r{^\[Unit\]$})
-          }
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                with_content(%r{^\[Service\]$})
+            }
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              without_content(%r{^\[Install\]$})
-          }
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                without_content(%r{^\[Unit\]$})
+            }
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              with_content(%r{^ExecStart=$})
-          }
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                without_content(%r{^\[Install\]$})
+            }
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              with_content(%r{^ExecStart=/usr/bin/doit.sh$})
-          }
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                with_content(%r{^ExecStart=$})
+            }
 
-          it {
-            is_expected.to contain_systemd__dropin_file('foobar.conf').
-              with_content(%r{^Type=oneshot$})
-          }
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                with_content(%r{^ExecStart=/usr/bin/doit.sh$})
+            }
+
+            it {
+              is_expected.to contain_systemd__dropin_file('foobar.conf').
+                with_content(%r{^Type=oneshot$})
+            }
+          end
         end
       end
     end
