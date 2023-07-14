4e3e6d2676a5292031e640111bb3e693314de887
Henry Hirsch <henry.hirsch@1und1.de>
2023-07-14 13:02:35 +0200

    copy spec tests

--- spec/type_aliases/systemd_unit_service_spec.rb
+++ spec/type_aliases/systemd_unit_service_spec.rb
@@ -79,4 +79,22 @@ describe 'Systemd::Unit::Service' do
   it { is_expected.to allow_value({ 'IOSchedulingPriority' => 7 }) }
   it { is_expected.to allow_value({ 'IOSchedulingPriority' => '' }) }
   it { is_expected.not_to allow_value({ 'IOSchedulingPriority' => '0' }) }
+
+  it { is_expected.to allow_value({ 'LimitMEMLOCK' => '100:100K' }) }
+
+  it { is_expected.to allow_value({ 'MemoryAccounting' => true }) }
+
+  it {
+    is_expected.to allow_value(
+      {
+        'MemoryLow' => '100',
+        'MemoryMin' => '10%',
+        'MemoryHigh' => '8G',
+        'MemoryMax' => 'infinity',
+        'MemorySwapMax' => '1T',
+      }
+    )
+  }
+
+  it { is_expected.not_to allow_value({ 'MemoryHigh' => '1P' }) }
 end
