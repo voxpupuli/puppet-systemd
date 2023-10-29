f341931d072d07193081f310a91f0eeb2b1fea01
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-10-29 23:24:11 +0100

    Assert systemd-resolved is installed

--- spec/acceptance/resolved_spec.rb
+++ spec/acceptance/resolved_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper_acceptance'
 
 describe 'systemd with manage_resolved true' do
+  has_package = (fact('os.family') == 'RedHat' && fact('os.release.major') != '8') || (fact('os.name') == 'Debian' && fact('os.release.major').to_i >= 12)
+
   context 'configure systemd resolved' do
     it 'works idempotently with no errors' do
       pp = <<-PUPPET
@@ -13,10 +15,7 @@ describe 'systemd with manage_resolved true' do
       PUPPET
       apply_manifest(pp, catch_failures: true)
       # RedHat 7, 9, Debian 12 and newer installs package first run before fact $facts['internal_services'] is set
-      if (fact('os.release.major') != '8' && (fact('os.family') == 'RedHat')) ||
-         ((fact('os.name') == 'Debian' && fact('os.release.major').to_i >= 12))
-        apply_manifest(pp, catch_failures: true)
-      end
+      apply_manifest(pp, catch_failures: true) if has_package
       apply_manifest(pp, catch_changes: true)
     end
 
@@ -24,6 +23,8 @@ describe 'systemd with manage_resolved true' do
       it { is_expected.to be_running }
       it { is_expected.to be_enabled }
     end
+
+    it { expect(package('systemd-resolved')).to be_installed } if has_package
   end
 
   context 'configure systemd stopped' do
