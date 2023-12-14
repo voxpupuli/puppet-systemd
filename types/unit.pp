b80a8ea5e392904d80df8d9b7f6db47ee7591533
Steve Traylen <steve.traylen@cern.ch>
2023-12-14 12:44:07 +0100

    Allow percent (%) character in unit names.
    This allows relations to be set up between similar instances of
    two templated units.
    
    For example:
    
    ```puppet
    systemd::manage_dropin{'user-aklog.conf':
      ensure     => present,
      unit       => 'user@.service',
      unit_entry => {
        'Documentation' => 'Run an aklog log before we start systemd --user',
        'After'         => ['user-aklog@%i.service'],
        'Requires'      => ['user-aklog@%i.service'],
      },
    }
    ```
    
    Previously the `%` character was considered illegal in a unit name.

--- types/unit.pp
+++ types/unit.pp
@@ -1,3 +1,3 @@
 # @summary custom datatype that validates different filenames for systemd units and unit templates
 # @see https://www.freedesktop.org/software/systemd/man/systemd.unit.html
-type Systemd::Unit = Pattern[/^[a-zA-Z0-9:\-_.\\@]+\.(service|socket|device|mount|automount|swap|target|path|timer|slice|scope)$/]
+type Systemd::Unit = Pattern[/^[a-zA-Z0-9:\-_.\\@%]+\.(service|socket|device|mount|automount|swap|target|path|timer|slice|scope)$/]
