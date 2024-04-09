voxpupuli/puppet-systemd

commit 988d2c7db2db38d42763352e62e70a91c631745c

Deprecate `systemd::service_limits`
The `systemd::service_limts` and corresponding type
`Systemd::Servicelimits` is deprecated.

Switch to `systemd::dropin_file` for a source attributed
`systemd::service_limits` or `systemd::manage_unit` for a
`limits` attributed `systemd::service_limits`.

manifests/service_limits.pp
@@ -1,4 +1,4 @@
-# Adds a set of custom limits to the service
+# Deprecated - Adds a set of custom limits to the service
 #
 # @api public
 #
@@ -47,13 +47,6 @@ define systemd::service_limits (
     fail('$name must match Pattern["^.+\.(service|socket|mount|swap)$"]')
   }
 
-  if $limits and !empty($limits) {
-    $_content = template("${module_name}/limits.erb")
-  }
-  else {
-    $_content = undef
-  }
-
   if $ensure != 'absent' {
     if ($limits and !empty($limits)) and $source {
       fail('You may not supply both limits and source parameters to systemd::service_limits')
@@ -63,14 +56,44 @@ define systemd::service_limits (
     }
   }
 
-  systemd::dropin_file { "${name}-90-limits.conf":
-    ensure                  => $ensure,
-    unit                    => $name,
-    filename                => '90-limits.conf',
-    path                    => $path,
-    selinux_ignore_defaults => $selinux_ignore_defaults,
-    content                 => $_content,
-    source                  => $source,
-    notify_service          => true,
+  if $limits {
+    # Some typing changed between Systemd::ServiceLimits and Systemd::Unit::Service
+    $_now_tupled = [
+      'IODeviceWeight',
+      'IOReadBandwidthMax',
+      'IOWriteBandwidthMax',
+      'IOReadIOPSMax',
+      'IOWriteIOPSMax',
+    ]
+
+    $_my_limits = $limits.map | $_directive, $_value | {
+      if $_directive in $_now_tupled {
+        { $_directive => $_value.map | $_pair | { Array($_pair).flatten } }  # Convert { 'a' => 'b' } to ['a', b']
+      } else {
+        { $_directive => $_value }
+      }
+    }.reduce | $_memo, $_value | { $_memo + $_value }
+
+    deprecation("systemd::servicelimits - ${title}",'systemd::servicelimits is deprecated, use systemd::manage_dropin')
+    systemd::manage_dropin { '#{name}-90-limits.conf':
+      ensure                  => $ensure,
+      unit                    => $name,
+      filename                => '90-limits.conf',
+      path                    => $path,
+      selinux_ignore_defaults => $selinux_ignore_defaults,
+      service_entry           => $_my_limits,
+      notify_service          => true,
+    }
+  } else {
+    deprecation("systemd::servicelimits ${title}",'systemd::servicelimits is deprecated, use systemd::dropin_file or systemd::manage_dropin')
+    systemd::dropin_file { '#{name}-90-limits.conf':
+      ensure                  => $ensure,
+      unit                    => $name,
+      filename                => '90-limits.conf',
+      path                    => $path,
+      selinux_ignore_defaults => $selinux_ignore_defaults,
+      source                  => $source,
+      notify_service          => true,
+    }
   }
 }

manifests/init.pp
@@ -6,7 +6,7 @@
 #   The default systemd boot target, unmanaged if set to undef.
 #
 # @param service_limits
-#   Hash of `systemd::service_limits` resources
+#   Deprecated, use dropin_files - Hash of `systemd::service_limits` resources
 #
 # @param networks
 #   Hash of `systemd::network` resources

spec/classes/init_spec.rb
@@ -335,6 +335,11 @@ describe 'systemd' do
             }
           end
 
+          # systemd::service_limits is deprecated
+          before do
+            Puppet.settings[:strict] = :warning
+          end
+
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_systemd__service_limits('openstack-nova-compute.service').with_limits('LimitNOFILE' => 32_768) }
         end

spec/defines/service_limits_spec.rb
@@ -10,6 +10,11 @@ describe 'systemd::service_limits' do
 
         let(:title) { 'test.service' }
 
+        # Whole type is deprecated but check it still works.
+        before do
+          Puppet.settings[:strict] = :warning
+        end
+
         describe 'with limits and present' do
           let(:params) do
             {

templates/limits.erb
@@ -1,16 +1,0 @@
-# This file managed by Puppet - DO NOT EDIT
-[Service]
-<%
-  @limits.keys.sort.each do |k|
-
-    # Handles the Path and Option entries
-    if @limits[k].is_a?(Array)
-      output = @limits[k].map{|x|
-        x = %(#{k}=#{x.to_a.flatten.join(' ')})
-      }.join("\n")
-   else
-     output = %(#{k}=#{@limits[k]})
-   end
--%>
-<%= output %>
-<% end -%>

types/servicelimits.pp
@@ -1,4 +1,5 @@
-# Matches Systemd Service Limit Struct
+# @summary Deprecated - Matches Systemd Service Limit Struct
+#
 type Systemd::ServiceLimits = Struct[
   {
     Optional['LimitCPU']            => Pattern['^\d+(s|m|h|d|w|M|y)?(:\d+(s|m|h|d|w|M|y)?)?$'],

commit a51bc397953410ec03323fcbd6ef58d52ecf54a4

Correctly interpolate variables in service_limits
Fixes: 988d2c7db2db ("Deprecate `systemd::service_limits`")

manifests/service_limits.pp
@@ -75,7 +75,7 @@ define systemd::service_limits (
     }.reduce | $_memo, $_value | { $_memo + $_value }
 
     deprecation("systemd::servicelimits - ${title}",'systemd::servicelimits is deprecated, use systemd::manage_dropin')
-    systemd::manage_dropin { '#{name}-90-limits.conf':
+    systemd::manage_dropin { "${name}-90-limits.conf":
       ensure                  => $ensure,
       unit                    => $name,
       filename                => '90-limits.conf',
@@ -86,7 +86,7 @@ define systemd::service_limits (
     }
   } else {
     deprecation("systemd::servicelimits ${title}",'systemd::servicelimits is deprecated, use systemd::dropin_file or systemd::manage_dropin')
-    systemd::dropin_file { '#{name}-90-limits.conf':
+    systemd::dropin_file { "${name}-90-limits.conf":
       ensure                  => $ensure,
       unit                    => $name,
       filename                => '90-limits.conf',