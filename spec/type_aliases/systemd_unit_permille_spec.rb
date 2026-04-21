4eed16a11ef1310082511d13477df9cd3057effe
Pat Riehecky <riehecky@fnal.gov>
2026-04-21 11:33:21 -0500

    Accept zero in Systemd::Unit::Percent, Permille, and Permyriad
    0%, 0‰, and 0‱ are all valid values per systemd.resource-control(5)
    and oomd.conf(5). ManagedOOMMemoryPressureLimit= explicitly documents
    0% as its default. percent.pp pattern already matched 0% correctly;
    only the spec and the two new types needed updating.

--- spec/type_aliases/systemd_unit_permille_spec.rb
+++ spec/type_aliases/systemd_unit_permille_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe 'Systemd::Unit::Permille' do
+  it { is_expected.to allow_value('0‰') }
   it { is_expected.to allow_value('1‰') }
   it { is_expected.to allow_value('500‰') }
   it { is_expected.to allow_value('1000‰') }
 
   it { is_expected.not_to allow_value(1) }
-  it { is_expected.not_to allow_value('0‰') }
   it { is_expected.not_to allow_value('1001‰') }
   it { is_expected.not_to allow_value('50%') }
   it { is_expected.not_to allow_value('50‱') }
--- types/unit/permyriad.pp
+++ types/unit/permyriad.pp
@@ -1,4 +1,4 @@
 # @summary Systemd definition of a permyriad value (‱, 1/10000)
 # @see https://www.freedesktop.org/software/systemd/man/oomd.conf.html
 #
-type Systemd::Unit::Permyriad = Pattern['\A([1-9][0-9]?[0-9]?[0-9]?|10000)‱\z']
+type Systemd::Unit::Permyriad = Pattern['\A(0|[1-9][0-9]?[0-9]?[0-9]?|10000)‱\z']
