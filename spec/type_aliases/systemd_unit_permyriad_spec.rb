7471f142b958e15aa5fc0bd046f9ad63d8237aae
Pat Riehecky <riehecky@fnal.gov>
2026-04-21 11:32:38 -0500

    Add Systemd::Unit::Permille and Systemd::Unit::Permyriad types
    Add type aliases for permille (‰, 1/1000) and permyriad (‱, 1/10000)
    fraction suffixes accepted by systemd resource-control and oomd.conf
    directives. Zero is not yet accepted; that follows in a separate commit.

---
+++ spec/type_aliases/systemd_unit_permyriad_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Systemd::Unit::Permyriad' do
+  it { is_expected.to allow_value('1‱') }
+  it { is_expected.to allow_value('5000‱') }
+  it { is_expected.to allow_value('10000‱') }
+
+  it { is_expected.not_to allow_value(1) }
+  it { is_expected.not_to allow_value('0‱') }
+  it { is_expected.not_to allow_value('10001‱') }
+  it { is_expected.not_to allow_value('50%') }
+  it { is_expected.not_to allow_value('50‰') }
+end
